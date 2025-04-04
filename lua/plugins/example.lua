952896a62597cc44cb847b98ddd59347d8495545
xxing <wxx1213383851@outlook.com>
2025-04-04 11:40:56 +0800

    update

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,6 +1,10 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then
+  return {
+    "github/copilot.vim"
+  }
+end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
